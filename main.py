42031c07392e0d42361774c8deb1a5eb367766ad
thegrimcoder <mbrust035@gmail.com>
2025-07-19 12:29:51 -0500

    AI Agent built using hard coded content

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+    model = "gemini-2.0-flash-001"
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model=model, contents=contents)
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
