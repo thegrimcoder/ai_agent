40dee7c821571c3488659e6181db0afdfb8e98df
thegrimcoder <mbrust035@gmail.com>
2025-07-20 13:18:24 -0500

    Using sys.argv for input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,11 +9,15 @@ client = genai.Client(api_key=api_key)
 
 def main():
     model = "gemini-2.0-flash-001"
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model=model, contents=contents)
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 1:
+        contents = sys.argv[1]
+        response = client.models.generate_content(model=model, contents=contents)
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print("No prompt was provided")
+        exit(1)
 
 if __name__ == "__main__":
     main()
