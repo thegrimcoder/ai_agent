d0dded136f57498f9b3fa95a1ae460066aba48af
thegrimcoder <mbrust035@gmail.com>
2025-07-21 17:30:28 -0500

    get_file_content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from config import *
 
 def get_files_info(working_directory, directory="."):
     try:
@@ -22,4 +23,21 @@ def get_files_info(working_directory, directory="."):
     
 
 def get_file_content(working_directory, file_path):
-    pass
+    try:
+        full_path = os.path.join(working_directory, file_path)
+        abs_full_path = os.path.abspath(full_path)
+        abs_working_directory = os.path.abspath(working_directory)
+        if not abs_full_path.startswith(abs_working_directory):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(abs_full_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        with open(abs_full_path, "r") as f:
+            if len(f.read) > 10000:
+                file_content_string = f.read(CHARACTER_LIMIT)
+                return file_content_string + f'[...File "{file_path}" truncated at 10000 characters]'
+            else:
+                return f.read()
+        
+    except Exception as e:
+        return f'Error: An unexpected error occured: {e}'
--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
-print(get_files_info("calculator", "."))
+print(get_file_content("calculator", "lorem.txt"))
 
-print(get_files_info("calculator", "pkg"))
+#print(get_files_info("calculator", "."))
 
-print(get_files_info("calculator", "/bin"))
+#print(get_files_info("calculator", "pkg"))
 
-print(get_files_info("calculator", "../"))
+#print(get_files_info("calculator", "/bin"))
+
+#print(get_files_info("calculator", "../"))
