9db80e2512ed483c4941a548248c1d3acceaa763
thegrimcoder <mbrust035@gmail.com>
2025-07-21 17:02:31 -0500

    Test File

---
+++ functions/get_files_info.py
@@ -0,0 +1,25 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        full_path = os.path.join(working_directory, directory)
+        abs_full_path = os.path.abspath(full_path)
+        abs_working_directory = os.path.abspath(working_directory)
+        if abs_full_path.startswith(abs_working_directory):
+            if os.path.isdir(full_path):
+                list = os.listdir(full_path)
+                results = "Result for current directory:\n"
+                for item in list:
+                    temp_path = os.path.join(full_path, item)
+                    results += f" - {item}: file_size={os.path.getsize(temp_path)} bytes, is_dir={os.path.isdir(temp_path)}\n"
+                return results
+            else:
+                return f'Error: "{directory}" is not a directory'
+        else:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    except Exception as e:
+        return f'Error: An unexpected error occured: {e}'
+    
+
+def get_file_content(working_directory, file_path):
+    pass
