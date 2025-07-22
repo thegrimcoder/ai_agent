3f10d36d8226e0cf03a2b1b32d9168ec7feeaaa1
thegrimcoder <mbrust035@gmail.com>
2025-07-22 18:20:56 -0500

    Complete run_python_file

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,2 +1,35 @@
 import os
+import subprocess
 
+def run_python_file(working_directory, file_path, args=[]):
+    try:
+        full_path = os.path.join(working_directory, file_path)
+        abs_full_path = os.path.abspath(full_path)
+        abs_working_directory = os.path.abspath(working_directory)
+        if not abs_full_path.startswith(abs_working_directory):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(abs_full_path):
+            return f'Error: File "{file_path}" not found.'
+        if not abs_full_path.endswith('.py'):
+            return f'Error: "{file_path}" is not a Python file.'
+        
+        if args:
+            execute_command = f"uv run {abs_full_path} {args[0]}"
+        else:
+            execute_command = f"uv run {abs_full_path}"
+        try:
+            completed_process = subprocess.run(execute_command, timeout=30, capture_output=True, text=True, shell=True)
+            output =""
+            if completed_process.stdout or completed_process.stderr:
+                output = f"STDOUT: \n{completed_process.stdout}\nSTDERR: \n{completed_process.stderr}"
+            else:
+                output = "No output produced"
+            if completed_process.returncode != 0:
+                output += f"\nProcess exited with code {completed_process.returncode}"
+
+            return output
+        except Exception as e:
+            return f"Error: executing Python file: {e}"
+        
+    except Exception as e:
+        return f'Error: An unexpected error occured while executing Python file: {e}'
--- tests.py
+++ tests.py
@@ -3,8 +3,11 @@ from functions.get_file_content import get_file_content
 from functions.write_file import write_file
 from functions.run_python_file import run_python_file
 
-
-
+print(run_python_file("calculator", "main.py"))
+print(run_python_file("calculator", "main.py", ["3 + 5"]))
+print(run_python_file("calculator", "tests.py"))
+print(run_python_file("calculator", "../main.py"))
+print(run_python_file("calculator", "nonexistent.py"))
 
 #print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 #print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
