878aab5205f117e1daca14d33827fbb6deba59a6
thegrimcoder <mbrust035@gmail.com>
2025-07-25 21:23:15 -0500

    Completed project

--- functions/call_function.py
+++ functions/call_function.py
@@ -7,35 +7,51 @@ from functions.run_python_file import run_python_file
 from functions.write_file import write_file
 from .config import WORKING_DIRECTORY
 
+functions = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "write_file": write_file,
+    "run_python_file": run_python_file
+}
 
 def call_function(function_call_part, verbose=False):
     verbose = verbose
 
-    if verbose == True:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-    else:
-        print(f" - Calling function: {function_call_part.name}")
-    
-    match function_call_part.name:
-        case get_files_info({}):
-            print("Running get_files_info...")
-            response = get_files_info(**function_call_part.args)
-        case get_file_content({}):
-            print("Running get_file_content...")
-            response = get_file_content(**function_call_part.args)
-        case write_file({}):
-            print("Running write_file...")
-            response = write_file(**fucntion_call_part.args) 
-        case run_python_files({}):
-            print("Running run_python...")
-            response = run_python_file(**function_call_part.args)
-        case _:
+    #Example of calling a function from the functions dictionary
+    #functions[function_call_part.name](**function_call_part.args)
+
+    if function_call_part.name in functions:
+        if verbose == True:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            result = functions[function_call_part.name](WORKING_DIRECTORY, **function_call_part.args)
             return types.Content(
                 role="tool",
                 parts=[
                     types.Part.from_function_response(
                         name=function_call_part.name,
-                        response={"error": f"Unknown function: {function_call_part.name}"},
+                        response={"result": result},
                     )
                 ],
-)
+            )
+        else:
+            print(f" - Calling function: {function_call_part.name}")
+            result = functions[function_call_part.name](WORKING_DIRECTORY, **function_call_part.args)
+            return types.Content(
+                role="tool",
+                parts=[
+                    types.Part.from_function_response(
+                        name=function_call_part.name,
+                        response={"result": result},
+                    )
+                ],
+            )
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from functions.get_file_content import schema_get_file_content
 from functions.write_file import schema_write_file
 from functions.run_python_file import schema_run_python_file
 from functions.config import SYSTEM_PROMPT
+from functions.call_function import call_function
 import sys
 
 load_dotenv()
@@ -27,16 +28,47 @@ def main():
     if len(sys.argv) > 1:
         contents = sys.argv[1]
         messages = [types.Content(role="user", parts=[types.Part(text=contents)]),]
-        response = client.models.generate_content(model=model, contents=messages, config=types.GenerateContentConfig(tools=[available_functions],system_instruction=SYSTEM_PROMPT))
-        if response.function_calls:
-            for functions_called in response.function_calls:
-                print(f"Calling function: {functions_called.name}({functions_called.args})")
-        else:
-            print(response.text)
-        if len(sys.argv) > 2:
-            print(f"User prompt: {contents}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        i = 0
+        while i <= 19:
+            try:
+                response = client.models.generate_content(model=model, contents=messages, config=types.GenerateContentConfig(tools=[available_functions],system_instruction=SYSTEM_PROMPT))
+                
+                if not response.function_calls and response.text:
+                    print(response.text)
+                    break
+
+                if response.candidates:
+                    for candidate in response.candidates:
+                        messages.append(candidate.content)
+                
+                if response.function_calls and len(sys.argv) > 2:
+                    for functions_called in response.function_calls:
+                        function_response = call_function(functions_called, True)
+                        try:
+                            if function_response.parts[0].function_response.response:
+                                msg = str(function_response.parts[0].function_response.response[next(iter(function_response.parts[0].function_response.response))])
+                                #print(f"-> {msg}")
+                                messages.append(msg)
+                            else:
+                                raise Exception("No response received")
+                        except Exception as e:
+                            print(f"An unexpected error occurred: {e}")
+                else:
+                    for functions_called in response.function_calls:
+                        function_response = call_function(functions_called, False)
+                        try:
+                            if function_response.parts[0].function_response.response:
+                                msg = str(function_response.parts[0].function_response.response[next(iter(function_response.parts[0].function_response.response))])
+                                #print(f"-> {msg}")
+                                messages.append(msg)
+                            else:
+                                raise Exception("No response received")
+                        except Exception as e:
+                            print(f"An unexpected error occurred: {e}")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+
+            i += 1
     else:
         print("No prompt was provided")
         exit(1)
